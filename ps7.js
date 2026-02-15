6ec11ae9ce9c9af8580ebd73469a422df34b2d02
HarshRaj2910 <harshroman617@gmail.com>
2026-02-15 16:00:56 +0530

    a

--- ps7.js
+++ ps7.js
@@ -1,3 +1,24 @@
 //  write an arrow function that returns the square of a number'n'.
 const sqr=(n)=> n*n;
 console.log(sqr(7));
+
+// write a function that prints "hello world" 5 times at intervals of 2s each.
+// const id=setInterval(()=>{
+//     console.log("Hello World!!");
+
+// },2000);
+// setTimeout(()=>{
+//     clearInterval(id);
+// },10000);
+
+
+let count = 0;
+
+const id = setInterval(() => {
+    count++;
+    console.log("Hello World!!");
+
+    if (count === 5) {
+        clearInterval(id);
+    }
+}, 2000);
